7ea7e9fbba3d80c4a5320d20c3719930a3c1832b
Yuichi Nakamura <y.naka.111.739@gmail.com>
2017-08-27 11:29:40 +0000

    201708272029

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -49,43 +49,6 @@
                                 </li>
                             </ul>
                         </li>
-                    @else                    @if (Auth::check())
-                        <li>
-                            <a href="#">
-                                <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
-                                アイテムを追加
-                              </a>
-                        </li>
-
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                                <span class="glyphicon glyphicon-signal" aria-hidden="true"></span>
-                                ランキング
-                                <span class="caret"></span>
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a href="{{ route('ranking.want') }}">Wantランキング</a></li>
-                            </ul>
-                        </li>
-
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                                <span class="gravatar">
-                                    <img src="{{ Gravatar::src(Auth::user()->email, 20) . '&d=mm' }}" alt="" class="img-circle">
-                                </span>
-                                {{ Auth::user()->name }}
-                                <span class="caret"></span>
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li>
-                                    <a href="{{ route('users.show', Auth::user()->id) }}">マイページ</a>
-                                </li>
-                                <li role="separator" class="divider"></li>
-                                <li>
-                                    <a href="{{ route('logout.get') }}">ログアウト</a>
-                                </li>
-                            </ul>
-                        </li>
                     @else
                         <li><a href="{{ route('signup.get') }}">新規登録</a></li>
                         <li><a href="{{ route('login.get') }}">ログイン</a></li>
