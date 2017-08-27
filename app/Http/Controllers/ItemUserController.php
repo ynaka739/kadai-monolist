3a681c0d67a358f03ddf46e29f2e84235268980a
Yuichi Nakamura <y.naka.111.739@gmail.com>
2017-08-27 11:03:32 +0000

    want

--- app/Http/Controllers/ItemUserController.php
+++ app/Http/Controllers/ItemUserController.php
@@ -7,7 +7,9 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
-class ItemsController extends Controller
+use App\Item;
+
+class ItemUserController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -26,33 +28,7 @@ class ItemsController extends Controller
      */
     public function create()
     {
-        $keyword = request()->keyword;
-        $items = [];
-        if ($keyword) {
-            $client = new \RakutenRws_Client();
-            $client->setApplicationId(env('RAKUTEN_APPLICATION_ID'));
-
-            $rws_response = $client->execute('IchibaItemSearch', [
-                'keyword' => $keyword,
-                'imageFlag' => 1,
-                'hits' => 20,
-            ]);
-
-            // 扱い易いように Item としてインスタンスを作成する（保存はしない）
-            foreach ($rws_response->getData()['Items'] as $rws_item) {
-                $item = new Item();
-                $item->code = $rws_item['Item']['itemCode'];
-                $item->name = $rws_item['Item']['itemName'];
-                $item->url = $rws_item['Item']['itemUrl'];
-                $item->image_url = str_replace('?_ex=128x128', '', $rws_item['Item']['mediumImageUrls'][0]['imageUrl']);
-                $items[] = $item;
-            }
-        }
-
-        return view('items.create', [
-            'keyword' => $keyword,
-            'items' => $items,
-        ]);
+        //
     }
 
     /**
@@ -74,13 +50,7 @@ class ItemsController extends Controller
      */
     public function show($id)
     {
-      $item = Item::find($id);
-      $want_users = $item->want_users;
-
-      return view('items.show', [
-          'item' => $item,
-          'want_users' => $want_users,
-      ]);
+        //
     }
 
     /**
@@ -115,5 +85,42 @@ class ItemsController extends Controller
     public function destroy($id)
     {
         //
+    }
+    
+        public function want()
+    {
+        $itemCode = request()->itemCode;
+
+        // itemCode から商品を検索
+        $client = new \RakutenRws_Client();
+        $client->setApplicationId(env('RAKUTEN_APPLICATION_ID'));
+        $rws_response = $client->execute('IchibaItemSearch', [
+            'itemCode' => $itemCode,
+        ]);
+        $rws_item = $rws_response->getData()['Items'][0]['Item'];
+
+        // Item 保存 or 検索（見つかると作成せずにそのインスタンスを取得する）
+        $item = Item::firstOrCreate([
+            'code' => $rws_item['itemCode'],
+            'name' => $rws_item['itemName'],
+            'url' => $rws_item['itemUrl'],
+            // 画像の URL の最後に ?_ex=128x128 とついてサイズが決められてしまうので取り除く
+            'image_url' => str_replace('?_ex=128x128', '', $rws_item['mediumImageUrls'][0]['imageUrl']),
+        ]);
+
+        \Auth::user()->want($item->id);
+
+        return redirect()->back();
+    }
+
+    public function dont_want()
+    {
+        $itemCode = request()->itemCode;
+
+        if (\Auth::user()->is_wanting($itemCode)) {
+            $itemId = Item::where('code', $itemCode)->first()->id;
+            \Auth::user()->dont_want($itemId);
+        }
+        return redirect()->back();
     }
 }
